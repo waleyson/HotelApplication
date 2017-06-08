cc5a3b7ac8cd371010102dacf9cf47f15132a153
waleyson <waleyson@yahoo.com>
2017-06-08 15:23:32 +0100

    Create forgotPassword.php

--- application/views/forgotPassword.php
+++ application/views/forgotPassword.php
@@ -2,7 +2,7 @@
 <html>
   <head>
     <meta charset="UTF-8">
-    <title>DigiLodge | Forgot Password</title>
+    <title>Fixsphere | Forgot Password</title>
     <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
     <link href="<?php echo base_url(); ?>assets/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
