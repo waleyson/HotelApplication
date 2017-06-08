3577225156d8d67836fff990b4238393de1604fe
waleyson <waleyson@yahoo.com>
2017-06-08 15:38:15 +0100

    Create header.php

--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -3,7 +3,6 @@
   <head>
     <meta charset="UTF-8">
     <title><?php echo $pageTitle; ?></title>
-    <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
     <!-- Bootstrap 3.3.4 -->
     <link href="<?php echo base_url(); ?>assets/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />    
     <!-- FontAwesome 4.3.0 -->
